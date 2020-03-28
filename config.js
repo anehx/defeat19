e45a68a97d065120c1047f8754d9aaeb43114676
Christian Zosel <christian@zosel.ch>
2020-03-28 15:54:11 +0100

    increase acceleration to 2

--- config.js
+++ config.js
@@ -4,14 +4,14 @@ module.exports = {
     size: 2000,
     grid: 20,
     maxSpeed: 7,
-    acceleration: 1
+    acceleration: 2,
   },
   player: {
-    size: 20
+    size: 20,
   },
   infection: {
     thresholdDistance: 150,
     speed: 1,
-    reduce: 0.1
-  }
+    reduce: 0.1,
+  },
 };
