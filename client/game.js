8d020fb5adb11e211d5cfd18a071f6b77ecd81fe
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2020-03-28 11:57:14 +0100

    refactoring

--- client/game.js
+++ client/game.js
@@ -1,4 +1,6 @@
 import * as PIXI from "pixi.js";
+import io from "socket.io-client";
+
 import Player from "./player";
 
 const { Application } = PIXI;
@@ -14,26 +16,62 @@ export default class Game extends Application {
     });
 
     this._players = {};
+
+    this.connect();
+
+    this.addResizeListeners();
+    this.addKeyboardListeners();
+    this.addSocketListeners();
   }
 
-  addPlayer(id) {
-    const player = new Player(id);
+  connect() {
+    this.socket = io("http://localhost:3000");
+  }
 
-    this._players[id] = player;
-    this.stage.addChild(player);
+  getPlayer(id) {
+    let player = this._players[id];
+
+    if (!player) {
+      // player does not exist yet, create a new one
+      player = new Player(id);
+
+      this._players[id] = player;
+      this.stage.addChild(player);
+    }
 
     return player;
   }
 
-  getPlayer(id) {
-    return this._players[id] || this.addPlayer(id);
+  setCenter([x, y]) {
+    this.center = { x, y };
   }
 
-  _resize() {
-    this.renderer.resize(window.innerWidth, window.innerHeight);
+  addResizeListeners() {
+    window.addEventListener("resize", () => {
+      this.renderer.resize(window.innerWidth, window.innerHeight);
+    });
   }
 
-  // render() {
-  //   Object.values(this._players).forEach(player => player._render());
-  // }
+  addKeyboardListeners() {
+    document.addEventListener("keydown", ({ code }) => {
+      if (/Arrow(Up|Down|Left|Right)/.test(code)) {
+        this.socket.emit("move", code.replace("Arrow", "").toLowerCase());
+      }
+    });
+  }
+
+  addSocketListeners() {
+    this.socket.on("hello", id => (this.playerId = id));
+
+    this.socket.on("update", ({ players }) => {
+      Object.entries(players).forEach(([id, { loc }]) => {
+        if (id == this.playerId) {
+          this.setCenter(loc);
+        }
+
+        const player = this.getPlayer(id);
+        player.setPosition(loc);
+      });
+    });
+  }
 }
--- client/index.js
+++ client/index.js
@@ -1,25 +1,3 @@
-import io from "socket.io-client";
 import Game from "./game";
 
-const socket = io("http://localhost:3000");
-
-const game = new Game();
-
-socket.on("hello", id => console.log(`Connected with ID ${id}`));
-
-socket.on("update", ({ players }) => {
-  Object.entries(players).forEach(([id, { loc }]) => {
-    const player = game.getPlayer(id);
-    player.setPosition(loc);
-  });
-});
-
-document.body.appendChild(game.view);
-
-window.addEventListener("resize", () => game._resize());
-
-document.addEventListener("keydown", ({ code }) => {
-  if (/Arrow(Up|Down|Left|Right)/.test(code)) {
-    socket.emit("move", code.replace("Arrow", "").toLowerCase());
-  }
-});
+document.body.appendChild(new Game().view);
