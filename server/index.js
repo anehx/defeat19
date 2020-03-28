fa9e17bc5bda7a41bbb482add1fdc1e2a55445bf
Christian Zosel <christian@zosel.ch>
2020-03-28 15:31:18 +0100

    serve static files

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-app.get("/", function (req, res) {
+app.get("/debug", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
+
+app.use(express.static(__dirname + "/../client/dist/"));
 
 const WORLD_SIZE = 2000;
 const MAX_SPEED = 7;
